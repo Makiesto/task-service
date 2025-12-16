60eb87ae5ecdf8ebda6cd55f46e900bb99ecc249
Mateusz <makiesto@gmail.com>
2025-12-16 20:47:26 +0100

    Fixed code in TaskRepository

--- task-service/src/main/java/com/taskmanagement/task_service/repository/TaskRepository.java
+++ task-service/src/main/java/com/taskmanagement/task_service/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.taskmanagement.task_service.repository;
 
 import com.taskmanagement.task_service.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
