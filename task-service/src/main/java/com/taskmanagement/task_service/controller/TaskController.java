f550ac15cd7f7ad3f0b532b55567219055a96050
Mateusz <makiesto@gmail.com>
2025-12-17 18:45:50 +0100

    Initial commit for RestController

--- task-service/src/main/java/com/taskmanagement/task_service/controller/TaskController.java
+++ task-service/src/main/java/com/taskmanagement/task_service/controller/TaskController.java
@@ -1,4 +1,23 @@
 package com.taskmanagement.task_service.controller;
 
+import com.taskmanagement.task_service.entity.Task;
+import com.taskmanagement.task_service.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpClient;
+import java.util.List;
+
+@RestController
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/")
+    public List<Task> getTasks() {
+
+        return taskService.findAllTasks();
+    }
 }
