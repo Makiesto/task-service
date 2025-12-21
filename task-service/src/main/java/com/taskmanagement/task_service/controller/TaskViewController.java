af7f64d42ba2c5e8769dbf9965912d318035c759
Mateusz <makiesto@gmail.com>
2025-12-21 19:51:34 +0100

    developed: add task functionality - get post, bug fix in updateTask method

--- task-service/src/main/java/com/taskmanagement/task_service/controller/TaskViewController.java
+++ task-service/src/main/java/com/taskmanagement/task_service/controller/TaskViewController.java
@@ -1,7 +1,8 @@
 package com.taskmanagement.task_service.controller;
 
 import com.taskmanagement.task_service.dto.TaskDTO;
-import com.taskmanagement.task_service.entity.Task;
+import com.taskmanagement.task_service.entity.Priority;
+import com.taskmanagement.task_service.entity.TaskStatus;
 import com.taskmanagement.task_service.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,8 +32,22 @@ public class TaskViewController {
         return "index";
     }
 
+    @GetMapping("/add")
+    public String showAddTask(Model model) {
+        TaskDTO task = new TaskDTO();
+
+        task.setPriority(Priority.MEDIUM);
+        task.setStatus(TaskStatus.TODO);
+
+        model.addAttribute("task", task);
+        model.addAttribute("priorities", Priority.values());
+        model.addAttribute("statuses", TaskStatus.values());
+
+        return "add-task";
+    }
+
     @PostMapping("/add")
-    public String addUser(@Valid TaskDTO task, BindingResult result, Model model) {
+    public String addTask(@Valid TaskDTO task, BindingResult result, Model model) {
         if (result.hasErrors()) {
             model.addAttribute("task", task);
 
@@ -42,6 +57,7 @@ public class TaskViewController {
         taskService.createTask(task);
         return "redirect:/index";
     }
+
 
     @GetMapping("/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
@@ -55,13 +71,14 @@ public class TaskViewController {
     public String updateTask(@PathVariable("id") long id, @Valid TaskDTO task,
                              BindingResult result) {
         if (result.hasErrors()) {
+            System.out.println("error occured in updateTask method");
             task.setId(id);
 
             return "update-task";
         }
 
         taskService.updateTask(id, task);
-        return "redirect:/index";
+        return "redirect:/tasks/index";
     }
 
     @GetMapping("/delete/{id}")
