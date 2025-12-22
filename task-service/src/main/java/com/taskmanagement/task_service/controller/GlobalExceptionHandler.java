f1ebd5655013470cf269b6bf4b1a2a7fc3c0bb4d
Mateusz <makiesto@gmail.com>
2025-12-22 10:14:30 +0100

    Code cleanup

--- task-service/src/main/java/com/taskmanagement/task_service/controller/GlobalExceptionHandler.java
+++ task-service/src/main/java/com/taskmanagement/task_service/controller/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.taskmanagement.task_service.controller;
 
 import com.taskmanagement.task_service.dto.TaskDTO;
+import com.taskmanagement.task_service.entity.Priority;
+import com.taskmanagement.task_service.entity.TaskStatus;
 import com.taskmanagement.task_service.exception.DeadlineBeforeTodayException;
 import com.taskmanagement.task_service.exception.DuplicateTitleException;
 import org.springframework.ui.Model;
@@ -10,21 +12,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(DuplicateTitleException.class)
+    @ExceptionHandler({
+        DuplicateTitleException.class,
+        DeadlineBeforeTodayException.class
+})
     public String handleDuplicateTitleException(Exception exception, Model model) {
 
         model.addAttribute("task", new TaskDTO());
+        model.addAttribute("priorities", Priority.values());
+        model.addAttribute("statuses", TaskStatus.values());
         model.addAttribute("errorMessage", exception.getMessage());
 
         return "add-task";
     }
 
-    @ExceptionHandler(DeadlineBeforeTodayException.class)
-    public String handleDeadlineBeforeTodayException(Exception exception, Model model) {
-
-        model.addAttribute("task", new TaskDTO());
-        model.addAttribute("errorMessage", exception.getMessage());
-
-        return "add-task";
-    }
 }
