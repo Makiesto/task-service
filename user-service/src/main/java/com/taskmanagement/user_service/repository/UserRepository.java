877da8aeb5156437d8bf88b3b7e463841c34c731
Mateusz <makiesto@gmail.com>
2025-12-29 19:37:31 +0100

    Refactor UserRepository to change method signature and add Repository annotation

--- user-service/src/main/java/com/taskmanagement/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/taskmanagement/user_service/repository/UserRepository.java
@@ -1,18 +1,19 @@
 package com.taskmanagement.user_service.repository;
 
-import com.taskmanagement.user_service.dto.UserResponseDTO;
 import com.taskmanagement.user_service.entity.User;
 import com.taskmanagement.user_service.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
 
-    Optional<List<User>> findByUserRole(UserRole role);
+    List<User> findByRole(UserRole role);
 }
