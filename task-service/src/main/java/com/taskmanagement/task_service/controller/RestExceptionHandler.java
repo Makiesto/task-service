a31fba9fddd0d36c55a9e1d319ed3702912c0b2a
Mateusz <makiesto@gmail.com>
2025-12-30 22:00:45 +0100

    Refactor RestExceptionHandler to use ApiErrorDTO for response entities

--- task-service/src/main/java/com/taskmanagement/task_service/controller/RestExceptionHandler.java
+++ task-service/src/main/java/com/taskmanagement/task_service/controller/RestExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.taskmanagement.task_service.controller;
 
-import com.taskmanagement.task_service.dto.ApiError;
+import com.taskmanagement.task_service.dto.ApiErrorDTO;
 import com.taskmanagement.task_service.exception.DeadlineBeforeTodayException;
 import com.taskmanagement.task_service.exception.DuplicateTitleException;
 import org.springframework.http.HttpStatus;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(DuplicateTitleException.class)
-    public ResponseEntity<ApiError> handleDuplicateTitle(DuplicateTitleException exception) {
+    public ResponseEntity<ApiErrorDTO> handleDuplicateTitle(DuplicateTitleException exception) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(new ApiError("DUPLICATE_TITLE", exception.getMessage()));
+                .body(new ApiErrorDTO("DUPLICATE_TITLE", exception.getMessage()));
     }
 
     @ExceptionHandler(DeadlineBeforeTodayException.class)
-    public ResponseEntity<ApiError> handleDeadline(DeadlineBeforeTodayException ex) {
+    public ResponseEntity<ApiErrorDTO> handleDeadline(DeadlineBeforeTodayException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ApiError("INVALID_DEADLINE", ex.getMessage()));
+                .body(new ApiErrorDTO("INVALID_DEADLINE", ex.getMessage()));
     }
 }
