d796e748566110c29118b7d46d47194dd99e29d1
Mateusz <makiesto@gmail.com>
2025-12-30 22:02:00 +0100

    Add UserUpdateEventDTO for user update events

---
+++ user-service/src/main/java/com/taskmanagement/user_service/dto/UserUpdateEventDTO.java
@@ -0,0 +1,15 @@
+package com.taskmanagement.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateEventDTO {
+    private String oldEmail;
+    private String newEmail;
+    private String firstName;
+    private String lastName;
+}
