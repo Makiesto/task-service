40f058bcf4741870370dfa664e75d540a37b6ce4
Mateusz <makiesto@gmail.com>
2025-12-30 22:04:09 +0100

    Add RabbitMQ configuration for user messaging

---
+++ user-service/src/main/java/com/taskmanagement/user_service/config/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package com.taskmanagement.user_service.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String EXCHANGE = "user_exchange";
+
+    @Bean
+    public TopicExchange userExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
