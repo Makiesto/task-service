4434ecae740bb98244e4a41a5b216fe74fc8c235
Mateusz <makiesto@gmail.com>
2025-12-30 22:04:17 +0100

    Add methods to TaskRepository for title existence check and task retrieval by email

--- task-service/src/main/java/com/taskmanagement/task_service/repository/TaskRepository.java
+++ task-service/src/main/java/com/taskmanagement/task_service/repository/TaskRepository.java
@@ -1,10 +1,16 @@
 package com.taskmanagement.task_service.repository;
 
 import com.taskmanagement.task_service.entity.Task;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     boolean existsByTitle(String title);
+
+    boolean existsByTitleAndIdNot(@NotBlank(message = "Title is required") String title, Long id);
+    List<Task> findByAssignedToEmail(String email);
 }
