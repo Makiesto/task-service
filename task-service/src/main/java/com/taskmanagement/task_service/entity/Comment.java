47d6b347c3da574fc10dc1f93e6e7a258944970c
Mateusz <makiesto@gmail.com>
2026-01-02 21:52:30 +0100

    Enhance Comment and Project entities with Lombok annotations and createdAt timestamp initialization

--- task-service/src/main/java/com/taskmanagement/task_service/entity/Comment.java
+++ task-service/src/main/java/com/taskmanagement/task_service/entity/Comment.java
@@ -1,13 +1,19 @@
 package com.taskmanagement.task_service.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,12 +22,17 @@ public class Comment {
     @Column(nullable = false)
     private String content;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
 
     private Long userId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "task_id", nullable = false)
     private Task task;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 }
--- task-service/src/main/java/com/taskmanagement/task_service/entity/Project.java
+++ task-service/src/main/java/com/taskmanagement/task_service/entity/Project.java
@@ -1,10 +1,7 @@
 package com.taskmanagement.task_service.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,9 +23,15 @@ public class Project {
 
     private String description;
 
-    @OneToMany(mappedBy="project", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    @ToString.Exclude
     private List<Task> tasks;
 
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 
 }
