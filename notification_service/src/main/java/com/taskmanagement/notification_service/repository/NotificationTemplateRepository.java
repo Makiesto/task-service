90ab090f45d77d722b35bf7baa0a3466d59f6d70
Mateusz <makiesto@gmail.com>
2026-01-02 17:57:11 +0100

    Update NotificationTemplateRepository to use Long as ID type and add method to find by NotificationType

--- notification_service/src/main/java/com/taskmanagement/notification_service/repository/NotificationTemplateRepository.java
+++ notification_service/src/main/java/com/taskmanagement/notification_service/repository/NotificationTemplateRepository.java
@@ -1,7 +1,11 @@
 package com.taskmanagement.notification_service.repository;
 
 import com.taskmanagement.notification_service.entity.NotificationTemplate;
+import com.taskmanagement.notification_service.entity.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationTemplateRepository extends JpaRepository<NotificationTemplate, Integer> {
+import java.util.Optional;
+
+public interface NotificationTemplateRepository extends JpaRepository<NotificationTemplate, Long> {
+    Optional<NotificationTemplate> findByType(NotificationType type);
 }
