440c5cfb0fdb80119320b4536dd3cfc713a110c8
Mateusz <makiesto@gmail.com>
2026-01-02 17:55:22 +0100

    Add userId field to UserUpdateEventDTO for enhanced user update tracking

--- user-service/src/main/java/com/taskmanagement/user_service/dto/UserUpdateEventDTO.java
+++ user-service/src/main/java/com/taskmanagement/user_service/dto/UserUpdateEventDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateEventDTO {
+    private Long userId;
     private String oldEmail;
     private String newEmail;
     private String firstName;
