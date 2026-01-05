f94fa3fd3e8e7dba4bada1e0cfb0a20de5edd0b8
Mateusz <makiesto@gmail.com>
2026-01-05 20:38:13 +0100

    Add mapping for team name in UserMapper to enhance user response DTOs

--- user-service/src/main/java/com/taskmanagement/user_service/mapper/UserMapper.java
+++ user-service/src/main/java/com/taskmanagement/user_service/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.taskmanagement.user_service.dto.UserRequestDTO;
 import com.taskmanagement.user_service.dto.UserResponseDTO;
 import com.taskmanagement.user_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
@@ -11,7 +12,11 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toEntity(UserRequestDTO dto);
+
+    @Mapping(source = "team.name", target = "teamName")
     UserResponseDTO toResponseDTO(User user);
+
+    @Mapping(source = "team.name", target = "teamName")
     List<UserResponseDTO> toResponseDTOList(List<User> users);
 
     void updateEntityFromDTO(UserRequestDTO userRequestDTO,  @MappingTarget User user);
