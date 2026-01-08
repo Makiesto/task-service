bace0868cf987828b5cb62299f567a2ed4c579d6
Mateusz <makiesto@gmail.com>
2026-01-08 23:56:51 +0100

    Add LoginResponseDTO for user authentication response

---
+++ backend/user-service/src/main/java/com/taskmanagement/user_service/dto/LoginResponseDTO.java
@@ -0,0 +1,20 @@
+package com.taskmanagement.user_service.dto;
+
+import com.taskmanagement.user_service.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponseDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private UserRole role;
+    private String message;
+}
